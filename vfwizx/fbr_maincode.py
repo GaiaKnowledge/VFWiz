158f270fee65f48de2e206bfa38be75fb3868eba
FrancisBaumont <44947452+FrancisBaumont@users.noreply.github.com>
2019-08-07 19:15:33 +0100

    v1 working model - JSON inputContainer issue?

--- vfwizx/fbr_maincode.py
+++ vfwizx/fbr_maincode.py
@@ -2,29 +2,31 @@
 
 import json
 import math
+import numpy as np
 
-from . vf_input import inputContainer
+from vf_inputs import Scenario
 
-def get_input_scenario(input_file):
-   with open(input_file) as f:
-       inputs = json.load(f)
-   input_scenario = inputContainer()
-   input_scenario.iLights = inputs['light_type']
-   input_scenario.iCrop = inputs['crop']
-   input_scenario.area = inputs['grow_area']
-   input_scenario.iSurface = inputs['surface_area']
-   input_scenario.iVolume = inputs['farm_volume']
-   input_scenario.iBuilding = inputs['building_type']
-   input_scenario.iSystem = inputs['grow_system']
-   input_scenario.iCO2 = inputs['co2_enrichment']
-   input_scenario.iEnergy = inputs['energy_price']
-   input_scenario.Toutdoors = inputs['average_outdoor_temperature']
-   input_scenario.iCrop_price = inputs['crop_price_per_kilo']
-   input_scenario.iWages = inputs['minimum_wage']
-   return input_scenario
+
+def get_scenario(input_file):
+    with open(input_file) as f:
+        inputs = json.load(f)
+    scenario = Scenario()
+    scenario.lights = inputs['lights']
+    scenario.crop = inputs['crop']
+    scenario.area = inputs['grow_area']
+    scenario.surface = inputs['surface_area']
+    scenario.volume = inputs['farm_volume']
+    scenario.building = inputs['building_type']
+    scenario.system = inputs['grow_system']
+    scenario.CO2 = inputs['co2_enrichment']
+    scenario.energy = inputs['energy_price']
+    scenario.toutdoors = inputs['average_outdoor_temperature']
+    scenario.crop_price = inputs['crop_price_per_kilo']
+    scenario.wages = inputs['minimum_wage']
+    return scenario
 
 # System
-def number_of_racks(grow_system, grow_area):
+def calc_no_of_racks(grow_system, grow_area):
    if grow_system == 'ziprack_8':
       no_of_racks = math.floor(grow_area/4.62963)  # 54 Zipracks per 250 sq-m (including aisles, work bench and plumbing kit)
    else:
@@ -33,63 +35,58 @@ def number_of_racks(grow_system, grow_area):
 
 # Harvest weight
 
-def crop(crop_type):
+def calc_harvest_weight(crop):
 
-   if crop_type == "lettuce":
+   if crop == "lettuce":
       harvest_weight = 0.5  # kg
    else:
-      harvest_weight = "unknown"
+      raise RuntimeError("Unknown crop {}".format(crop))
+
    return harvest_weight
 
-def gross_yield(crop_type):
-   if crop_type == 'lettuce':
+def get_gross_yield(crop):
+   if crop == 'lettuce':
       ys = 78.5  # kg / m2 / year
    else:
-      ys = 'unknown'
+      raise RuntimeError("Unknown crop {}".format(crop))
    return ys
 
 
 # Plant Capacity
 
-def plant_capacity(crop_type, grow_system, no_of_racks):   # Excluding propagation and only those within the vertical farming systems
-
-   if crop_type == "lettuce" and grow_system == 'ziprack_8':
+def calc_plant_capacity(crop, grow_system, no_of_racks):   # Excluding propagation and only those within the vertical farming systems
+   if crop == "lettuce" and grow_system == 'ziprack_8':
       no_of_towers = no_of_racks*30  # Tight spacing with lettuce (30 towers per rack)
       yield_capacity = no_of_towers*3.3  # 3.3kg of greens per tower
-      farm_plant_capacity = yield_capacity/harvest_weight # Potential yield divided by harvest weight of each product
-   elif grow_system != 'ziprack_8' or crop_type != 'lettuce':
-      print("unknown system or crop variety. Please insert another.")
-      yield_capacity = "unknown"
-      farm_plant_capacity = "unknown"
+      harvest_weight = calc_harvest_weight(crop)
+      farm_plant_capacity = yield_capacity / harvest_weight # Potential yield divided by harvest weight of each product
    else:
-      print("unknown")
-      yield_capacity = "unknown"
-      farm_plant_capacity = "unknown"
+      raise RuntimeError("Unknown crop {}".format(crop))
+
    return farm_plant_capacity, yield_capacity
 
 
 # Lights
 
-def displayspec(light_type):
-    if light_type == "intraspectra_spectrablade_8":
+def get_spec(lights):
+    if lights == "intraspectra_spectrablade_8":
       light_wattage = 75
       light_efficiency = 0.4
-      print('The' + light_type + 'light is' + light_wattage, "Watts with an efficiency of:" + light_efficiency)
+      print("The {} light is {} watts with an efficiency of: {}".format(lights, light_wattage, light_efficiency))
     else:
-      light_wattage = 'unknown'
-      light_efficiency = 'unknown'
+         raise RuntimeError("Unknown lights {}".format(lights))
     return light_wattage, light_efficiency
 
-def get_qty_lights(grow_system, no_of_racks):
+def calc_no_of_lights(grow_system, no_of_racks):
    if grow_system == 'ziprack_8':
-      no_of_lights == no_of_racks*24  # Assumption that 24 lighting units are require to cover crop area of 1 Ziprack (30 towers)
+      no_of_lights = no_of_racks*24  # Assumption that 24 lighting units are require to cover crop area of 1 Ziprack (30 towers)
    else:
-      no_of_lights == "unknown"
+      raise RuntimeError("Unknown grow system {}".format(grow_system))  
    return no_of_lights
 
-def get_lights_energy(light_type, qty_lights):
-   lights_watts, efficiency = displayspec(light_type)
-   lighting_kw_usage = lights_watts*qty_lights/1000
+def calc_lights_energy(lights, no_of_lights):
+   lights_watts, efficiency = get_spec(lights)
+   lighting_kw_usage = lights_watts*no_of_lights/1000
    kwh_per_day = lighting_kw_usage*12  # Assuming 12 hours of light for plants
    return kwh_per_day
 
@@ -97,14 +94,14 @@ def get_lights_energy(light_type, qty_lights):
 
 # HVAC
 
-def temp_crop_reqs(crop_type):
-   if crop_type == 'lettuce':
+def get_temp_crop_reqs(crop):
+   if crop == 'lettuce':
       Tin = 23.9  # Temperature optimal for lettuce growth
    else:
       Tin = 22  # 'general temperature'
    return Tin
 
-def HVAC_energy(surface_area, building_type, Tin, Tout):
+def calc_HVAC_energy(surface_area, building_type, Tin, Tout):
    if building_type == 'basement':
       U = 0.5
    else:
@@ -131,16 +128,13 @@ def HVAC_energy(surface_area, building_type, Tin, Tout):
 # Labour
 
 # Energy
-def daily_energy_consumption(HVAC_daily_energy, lights_daily_energy):
+def calc_daily_energy_consumption(HVAC_daily_energy, lights_daily_energy):
    kwh_per_day = HVAC_daily_energy + lights_daily_energy
    return kwh_per_day
 
-
-)
-
-def monthly_energy_consumption(farm_kwh_per_day):
-   m_energy_consumption = farm_kwh_per_day * 28  # 4 weeks, 28 days a month
-   return mec
+def calc_monthly_energy_consumption(farm_kwh_per_day):
+   monthly_energy_consumption_farm = farm_kwh_per_day * 28  # 4 weeks, 28 days a month
+   return monthly_energy_consumption_farm
 
 
 # Yield
@@ -161,19 +155,19 @@ Notes
 
 
 
-def get_crop_ppfd_reqs(crop_type):
-   if crop_type == 'lettuce':
+def calc_crop_ppfd_reqs(crop):
+   if crop == 'lettuce':
       crop_ppfd_reqs = 295
    else:
-      crop_ppfd_reqs = 'unknown'
+      raise RuntimeError("Unknown crop: {}".format(crop))
    return crop_ppfd_reqs
 
 
 # Annual yield
-def adjusted_yield(building_type, ys, crop_type, pa, light_type, co2_enrichment, tf, crop_ppfd_reqs, ppfd_lights, grow_area):
+def calc_adjusted_yield(building_type, ys, crop, pa, lights, co2_enrichment, tf, crop_ppfd_reqs, ppfd_lights, grow_area):
 
    #PAR factor
-   if light_type == "intraspectra_spectrablade_8":
+   if lights == "intraspectra_spectrablade_8":
       PARf = 1
    else:
       PARf = ppfd_lights/crop_ppfd_reqs  # ratio of PAR delivered to canopy to theoretical PAR reqs
@@ -181,13 +175,13 @@ def adjusted_yield(building_type, ys, crop_type, pa, light_type, co2_enrichment,
    if co2_enrichment == 'yes':
       CO2f = 1
    else:
-      CO2f = 'unknown'
+      CO2f = 0.9
 
    # standard yield
    if isinstance(ys, int):
       ys = ys
    else:
-      ys = gross_yield(crop_type)
+      ys = get_gross_yield(crop)
       pa = grow_area
 
    #Temperature
@@ -205,41 +199,15 @@ def sales(ya, crop_price):
    return crop_sales
 
 
-
-input_file = 'input_file.json'
-scenario = get_input_scenario(input_file)
-
-no_of_racks = number_of_racks(scenario.iSystem, scenario.area)
-qty_lights = get_qty_lights(scenario.iSystem, no_of_racks)
-lights_daily_energy = get_lights_energy(scenario.iLights, qty_lights)
-
-HVAC_daily_energy = HVAC_energy(surface_area=scenario.iSurface, building_type=scenario.iBuilding,
-                                Tin=temp_crop_reqs(scenario.iCrop, Tout=scenario.Toutdoors))
-farm_kwh_per_day = daily_energy_consumption(HVAC_daily_energy, lights_daily_energy
-monthly_energy_consumption_farm = monthly_energy_consumption(farm_kwh_per_day)
-farm_plant_capacity, standard_yield = plant_capacity(scenario.iCrop, scenario.iSystem, no_of_racks)
-ys = standard_yield
-crop_ppfd_reqs = get_crop_ppfd_reqs(scenario.iCrop)
-ppfd_lights = 295  # placeholder
-
-tf = 1
-
-# OPEX
-
-OpEx: int = 0
-days = 366
-print("Days",days-1)
-OpEx_array = []
-
-def OpEx(days,labour daily_energy_consumption):
+def OpEx(days,labour, farm_kwh_per_day, energy_price):
    for i in range(days):
       if i % 30 == 0:
        # OpEx += labour()  # Fixed costs
-       # OpEx += energy()
+       OpEx += (energy(farm_kwh_per_day, energy_price)*30)
 
       elif i % 365 == 0:
         OpEx += 2 # annualcosts(ienergystandingcharge, iwaterstandingcharge, iinsurance)  # Fixed costs
-    OpEx_array.append(OpEx)
+
 
 # Operations = Bill Growth Lights + Bill Environmental Control + Bill Misc Energy + Water Bill + Seed Cost
 # + Nutrient Cost + Personnel Cost + Maintenance Cost + CO2 Cost - Reduction from Renewable Energy
@@ -247,24 +215,20 @@ def OpEx(days,labour daily_energy_consumption):
 
 # REVENUE
 
-sales: int = 0
-sales_array = []
 
-for i in range(days):
-    if i % 30 == 0:
-        sales += sales(ya, crop_price)) # revenue(input_data.iSystem)
+
+
+def revenue(days, ya, crop_price):
+   sales = 0.0
+   for i in range(days):
+      if i % 30 == 0:
+        sales += sales(ya, crop_price) # revenue(input_data.system)
         #Revenue += rent(iAnnual_rent, iSize, iLocation, iLocation_type)
         #Revenue += utilitiesM(energy(), water(), internet)
         #Revenue += consumables(nutrients, seeds, grow_media)
         #Revenue +=
-    elif i % 365 == 0:
+      elif i % 365 == 0:
         sales += 50
-    sales_array.append(sales)
-
-# ARRAY conversion
-sales_array = np.asarray(sales_array) # Sales as an array
-OpEx_array = np.asarray(OpEx_array)  # OpEx as an array
-
 
 # PROFIT
 def profit(sales_array, OpEx_array):
@@ -272,23 +236,53 @@ def profit(sales_array, OpEx_array):
    return profit_array
 
 
-profit_array = profit(sales_array, OpEx_array)
-
-
 def gross_profit_margin(sales_array,
                         cogs):  # Profit and Cost of Goods Sold - i.e. cost of materials and director labour costs
    gross_profit_margin = (sales_array - cogs) / sales_array  # Total revenue - Cost of goods sold (COGS) / revenue
    return gross_profit_margin
+
+# OPEX
+
+OpEx: int = 0
+days = 366
+OpEx_array = []
+sales: int = 0
+sales_array = []
+
+print("Days",days-1)
+input_file = 'input_file.json'
+scenario = get_scenario(input_file)
+
+no_of_racks = calc_no_of_racks(scenario.system, scenario.area)
+no_of_lights = calc_no_of_lights(scenario.system, no_of_racks)
+lights_daily_energy = calc_lights_energy(scenario.lights, no_of_lights)
+
+HVAC_daily_energy = calc_HVAC_energy(surface_area=scenario.surface, building_type=scenario.building,
+                                Tin=get_temp_crop_reqs(scenario.crop), Tout=scenario.toutdoors)
+farm_kwh_per_day = calc_daily_energy_consumption(HVAC_daily_energy, lights_daily_energy)
+monthly_energy_consumption_farm = calc_monthly_energy_consumption(farm_kwh_per_day)
+farm_plant_capacity, standard_yield = calc_plant_capacity(scenario.crop, scenario.system, no_of_racks)
+ys = standard_yield
+crop_ppfd_reqs = calc_crop_ppfd_reqs(scenario.crop)
+ppfd_lights = 295  # placeholder
+
+tf = 1
+OpEx_array.append(OpEx)
+# ARRAY conversion
+sales_array.append(sales)
+sales_array = np.asarray(sales_array) # Sales as an array
+OpEx_array = np.asarray(OpEx_array)  # OpEx as an array
+profit_array = profit(sales_array, OpEx_array)
 
 
 # gross_profit_margin(sales_array, cogs)
 
 print("Profit £:", profit_array[-1])
 
-plt.plot(profit_array)
-plt.xlabel('Days')
-plt.ylabel('Gross Profit')
-plt.show()
+# plt.plot(profit_array)
+# plt.xlabel('Days')
+# plt.ylabel('Gross Profit')
+# plt.show()
 
 # plt.figure()
 # plt.plot(gross_profit_margin)
